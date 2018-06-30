c0ae6a46edd381a99a9d34d8aa25b4d4ee4e7993
Tang Jianhua <shmp2005@163.com>
2018-06-30 17:26:32 +0800

    adding config

--- src/main/java/com/zhongba/jpaer/JpaerApplication.java
+++ src/main/java/com/zhongba/jpaer/JpaerApplication.java
@@ -2,8 +2,10 @@ package com.zhongba.jpaer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class JpaerApplication {
 
     public static void main(String[] args) {
